f9ca637addb31984c56e4197688ab9e824d1cf4b
Amir9eng <amirmukhtarme@gmail.com>
2022-10-20 19:21:12 +0100

    search filters and routing

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { MdCancel } from 'react-icons/md'
+import Image from 'next/image'
+import {
+  Flex,
+  Box,
+  Select,
+  Icon,
+  Button,
+  Input,
+  Spinner,
+  Text
+} from '@chakra-ui/react'
+import { filterData, getFilterValues } from '../utils/fetchData'
 
 const SearchFilters = () => {
-  return <div></div>
+  const [filters, setFilters] = useState(filterData)
+  const router = useRouter()
+
+  const searchProperties = filterValues => {
+    const path = router.pathname
+    const { query } = router
+
+    const values = getFilterValues(filterValues)
+
+    values.forEach(item => {
+      if (item.value && filterValues?.[item.name]) {
+        query[item.name] = item.value
+      }
+    })
+    router.push({ pathname: path, query })
+  }
+
+  return (
+    <Flex bg='gray.100' flexWrap='wrap' p='4' justifyContent='center'>
+      {filters.map(filter => (
+        <Box key={filter.queryName}>
+          <Select
+            placeholder={filter.placeholder}
+            w='fit-content'
+            p='2'
+            onChange={e =>
+              searchProperties({ [filter.queryName]: e.target.value })
+            }
+          >
+            {filter?.items?.map(item => (
+              <option value={item.value} key={item.key}>
+                {item.name}
+              </option>
+            ))}
+          </Select>
+        </Box>
+      ))}
+    </Flex>
+  )
 }
 
 export default SearchFilters
--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -6,7 +6,7 @@ export const fetchApi = async(url) => {
     const { data } = await axios.get(url, {
         headers: {
             "x-rapidapi-host": "bayut.p.rapidapi.com",
-            "x-rapidapi-key": "385a72c8e2mshb9b150fa57090a9p116e07jsn409763e6b175",
+            "x-rapidapi-key": process.env.NEXT_PUBLIC_RAPID_API_KEY,
         },
     });
     return data;
